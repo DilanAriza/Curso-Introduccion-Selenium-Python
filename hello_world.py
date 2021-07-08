08cce22b4fcd0a8e505802c54813288ff0b79fff
DilanAriza <dilansantiagoariza@gmail.com>
2021-07-07 23:30:49 -0500

    Añadiendo todo al primer commit

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox(executable_path = r"./geckodriver.exe")
+        driver = cls.driver
+        driver.implicitly_wait(10)
+    
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get("https://platzi.com")
+
+    def test_visit_wikipedia(self):
+        self.driver.get("https://www.wikipedia.org")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(
+        verbosity=2, 
+        testRunner=HTMLTestRunner(
+            output='reportes', 
+            report_name="hello-world-report",
+        )
+    )
